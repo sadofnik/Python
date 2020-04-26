950cb711d5d2d51de090856570cee53ea8aeeac7
alexander <alex@example.com>
2020-04-26 19:38:45 +0300

    added les07 end

--- Lesson_7/practice_7.1.py
+++ Lesson_7/practice_7.1.py
@@ -1,1 +1,27 @@
- 
+class Matrix:
+
+    def __init__(self, param1):
+        self.par1 = param1
+
+    """Вывод матрицы красивый"""
+
+    def __str__(self):
+        return '\n'.join(['\t'.join([str(i) for i in el]) for el in self.par1])
+
+    """Вытаскиваем каждые значения списка в цикле и сохраняем во временном списке."""
+
+    def __add__(self, other):
+        matrix = []
+        for i in range(len(self.par1)):
+            a = []
+            for el in range(len(self.par1[i])):
+                a.append(self.par1[i][el] + other.par1[i][el])
+            matrix.append(a)
+        return Matrix(matrix)
+
+
+c1 = Matrix([[10, 11, 12], [12, 13, 12], [3, 4, 12]])
+c2 = Matrix([[1, 12, 2], [1, 12, 2], [1, 3, 12]])
+print(f"Первая матрица:\n{c1}")
+print(f"Вторая матрица:\n{c2}")
+print(f"Сумма матриц:\n{c1 + c2}")
---
+++ Lesson_7/practice_7.2.py
@@ -0,0 +1,42 @@
+from abc import ABC, abstractmethod
+
+
+class Clothes(ABC):
+
+    def __init__(self, clot_name, size):
+        self.name = clot_name
+        self.size = size
+
+    @abstractmethod
+    def expend(self):
+        pass
+
+    def __add__(self, other):
+        return self.expend + other.expend
+
+
+class Coat(Clothes):
+    def __init__(self, clot_name, coat_size):
+        super().__init__(clot_name, size=coat_size)
+        self.V = coat_size
+
+    @property
+    def expend(self):
+        return self.V / 6.5 + 0.5
+
+
+class Costume(Clothes):
+    def __init__(self, clot_name, costume_size):
+        super().__init__(clot_name, size=costume_size)
+        self.H = costume_size
+
+    @property
+    def expend(self):
+        return self.H * 2 + 0.3
+
+
+a = Coat("Летнее пальто", 22.75)
+print(a.expend)
+b = Costume("Костюм Тройка", 1.35)
+print(b.expend)
+print(f"Суммарный расход ткани: {a + b}")
